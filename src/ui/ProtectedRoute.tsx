91895ca45bd26a89faa8bc38ad13bc2634756b41
kareem <124769561+Kareemmmfayed@users.noreply.github.com>
2024-04-27 19:35:10 +0200

    Added authorization

---
+++ src/ui/ProtectedRoute.tsx
@@ -0,0 +1,36 @@
+import styled from "styled-components";
+import { useUser } from "../features/authentication/useUser";
+import Spinner from "./Spinner";
+import { useNavigate } from "react-router-dom";
+import { useEffect } from "react";
+
+const FullPageSpinner = styled.div`
+  height: 100vh;
+  background-color: var(--color-grey-50);
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+
+function ProtectedRoute({ children }: { children: React.ReactNode }) {
+  const navigate = useNavigate();
+
+  const { isLoading, isAuthenticated } = useUser();
+
+  useEffect(() => {
+    if (!isAuthenticated && !isLoading) {
+      navigate("/login");
+    }
+  }, [isAuthenticated, isLoading, navigate]);
+
+  if (isLoading)
+    return (
+      <FullPageSpinner>
+        <Spinner />
+      </FullPageSpinner>
+    );
+
+  if (isAuthenticated) return children;
+}
+
+export default ProtectedRoute;
