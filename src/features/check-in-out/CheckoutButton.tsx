0fd18dd36cca072dce6933bea44c898813593e4e
kareem <124769561+Kareemmmfayed@users.noreply.github.com>
2024-05-13 23:44:00 +0200

    Fixed some bugs

--- src/features/check-in-out/CheckoutButton.tsx
+++ src/features/check-in-out/CheckoutButton.tsx
@@ -1,8 +1,16 @@
 import Button from "../../ui/Button";
+import { useCheckout } from "./useCheckout";
 
-function CheckoutButton({ bookingId }) {
+function CheckoutButton({ bookingId }: { bookingId: string | number }) {
+  const { checkout, isCheckingOut } = useCheckout();
+
   return (
-    <Button variation="primary" size="small">
+    <Button
+      variation="primary"
+      size="sm"
+      onClick={() => checkout(+bookingId)}
+      disabled={isCheckingOut}
+    >
       Check out
     </Button>
   );
